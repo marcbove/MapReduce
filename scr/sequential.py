afddffe4a182d4e9494e46c53e42c3e81e8b35a0
Marc Bove <mbg@localhost.localdomain>
2018-03-07 11:55:11 +0100

    Sequencial

--- scr/sequential.py
+++ scr/sequential.py
@@ -1,13 +1,21 @@
-import sys
-
+import sys, pprint
 
 if __name__ == "__main__":
   if len(sys.argv) != 2:
     print 'bad arguments'
     exit()
 
-  path = sys.argv[2]
+  dictionary = {}
+  path = sys.argv[1]
+  with open(path, 'r') as file:
+  	for line in file:
+  		for word in line.split():
+  			word = filter(lambda x: x.isalnum(), word)
+	  		if dictionary.has_key(word):
+	  			dictionary[word] += 1 
+	  		else:
+	  			dictionary[word]= 1
 
-  file = open(path, 'r')
-
+  pprint.pprint(dictionary.items())
+  print "Nombre paraules", len(dictionary)
   
