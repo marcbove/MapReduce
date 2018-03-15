f1ab967c7dc1215aca01929c07002f31cc9b7cfa
Christian C. Romero <id.32@outlook.com>
2018-03-15 11:27:30 +0100

    style fixes

--- mapreduce/host.py
+++ mapreduce/host.py
@@ -2,30 +2,30 @@ from pyactor.context import set_context, create_host, serve_forever
 
 
 def create_host_port():
-  port = 6001
-  while True:
-    try:
-      return create_host('http://127.0.0.1:{}/'.format(port))
-    except Exception as e:
-      print(e)
-      port += 1
+    port = 6001
+    while True:
+        try:
+            return create_host('localhost:{}/'.format(port))
+        except Exception as e:
+            print(e)
+            port += 1
 
 
 if __name__ == '__main__':
-  PORT = 6001
+    PORT = 6001
 
-  set_context()
+    set_context()
 
-  host = create_host_port()
+    host = create_host_port()
 
-  registry = host.lookup_url('http://127.0.0.1:6000/regis', 'Registry', 'registry')
+    registry = host.lookup_url('localhost:6000/regis', 'Registry', 'registry')
 
-  name = 'host' + str(len(registry.get_all()))
+    name = 'host{}'.format(len(registry.get_all()))
 
-  registry.bind(name, host)
+    registry.bind(name, host)
 
-  pprint.pprint(registry.get_all())
+    pprint.pprint(registry.get_all())
 
-  print('host listening at port {}'.format(PORT))
+    print('host listening at port {}'.format(PORT))
 
-  serve_forever()
+    serve_forever()
