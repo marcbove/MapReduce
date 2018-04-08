485ef4487d89161bea2ce1034e94b52a6fc45648
Christian C. Romero <id.32@outlook.com>
2018-04-08 20:45:15 +0200

    Added dummy tests
    Co-authored-by: Marc <in.smx.82@gmail.com>

---
+++ mapreduce/main-mapreduce.py
@@ -0,0 +1,23 @@
+import SimpleHTTPServer
+import SocketServer
+import os
+import thread
+
+
+def startServer():
+  PORT = 8000
+
+  web_dir = os.path.join(os.path.dirname(__file__), 'web')
+  os.chdir(web_dir)
+
+  Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+  httpd = SocketServer.TCPServer(('', PORT), Handler)
+  print('serving at port {}'.format(PORT))
+  print(web_dir)
+  httpd.serve_forever()
+  print('hey')
+
+
+if __name__ == '__main__':
+  thread.start_new_thread(startServer,())
+  print('hi')
---
+++ tests.py
@@ -0,0 +1,12 @@
+import unittest
+
+import mapreduce
+
+
+class TestMethods(unittest.TestCase):
+  def test_add(self):
+    self.assertEqual('Hello Wolrd!', 'Hello Wolrd!')
+
+
+if __name__ == '__main__':
+  unittest.main()
